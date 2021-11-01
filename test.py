e97d4ae35f16581c700e909fd0a37c4fdffce573
AnasZaghloul <82937499+AnasZaghloul@users.noreply.github.com>
2021-11-01 18:03:15 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+import time
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv("full_2020_reduit.csv")[:10000]
+
+df
+
